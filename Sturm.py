d27c2c5e8391eafe343b72862cdd1934bad48297
serhrnz <89656547+serhrnz@users.noreply.github.com>
2021-09-21 23:41:04 -0500

    Update Sturm.py

--- Sturm.py
+++ Sturm.py
@@ -1,6 +1,6 @@
-#Determinamos el resultado del polinimio respecto a x
+#Se define la función HORNER que evalúa el polinomio p en el valor z
 def HORNER(p, z):
-    #Si un único p es igual a uno se deben de cumplir las determinadas caracaterísticas
+    #Si la longitud de p es mayor a 1, se realiza el algoritmo de manera normal
     if len (p)!= 1:
         n = len(p)-1
         x = p[0]
@@ -10,34 +10,33 @@ def HORNER(p, z):
             x = p[i]+z*x
             y = x+z*y
         x = p[n]+z*x
-        #Obtenemos el valor de x
+        #Obtenemos el valor del polinomio evaluado en z
         return x
     else:
-        #Si no se cumple un único p igual a 1 regresamos p
+        #Si la longitud de p es 1, significa que en realidad es una constante, ya que es de grado 0, por lo que únicamente pediremos que nos regrese el valor del único elemento
         return float(p[0])
 
-#Derivamos el polinimio 
+#Se define la función DERIVAR que nos devuelve la derivada del polinomio p
 def DERIVAR (p):
     pp = []
     for i in range (len(p)-1,0,-1):
         pp.append(p[i]*i)
     pp = list(reversed(pp))
-    #Obtenemos el resultado de la derivada
+    #Obtenemos la derivada
     return pp
 
-#Dividimos el polinimio y su derivada
+#Dividimos el polinimio entre su derivada
 def DIVIDIR (p, pp):
-    #Si se cumplen q y s continuamos el proceso
     while True:
         q = 0
         s = p[len(p)-1]/pp[len(pp)-1]
-        #Asignamos un rango para i
+        #Asignamos el rango de iteraciones para i
         for i in range (len(pp)-1,-1,-1):
             t = s*pp[i]
             w = p[(len(p)-1)-q]-t
             p[(len(p)-1)-q]=w
             q = q+1
-        #Obtenemos el valor de i
+        #Asignamos a i el valor del grado del polinomio
         i = len(p)-1
         while p [i]==0:
             p.pop(i)
@@ -45,47 +44,46 @@ def DIVIDIR (p, pp):
         #Si el polinomio es menor que la derivada detenemos el procedimiento
         if len(p)<len(pp):
             break
-    #Obtenemos el resultado de p para j
     p=[j*-1 for j in p]
     return p
 
 def STURM (p,izq,der):
-    #Primero se ordenan los coeficientes del polinomio inicial en P0, de menor a mayor grado
+    #Se declaran las listas que contendrán los resultados de los polinomios obtenidos, evaluados en los extremos izquierdo y derecho de nuestro intervalo
     valoresizq=[]
     valoresder=[]
     print ("\nP0:\n",p)
+    #Se agregan a cada lista el resultado de evaluar p0 en los extremos
     valoresizq.append(HORNER(list (reversed(p)),izq))
     valoresder.append(HORNER(list (reversed(p)),der))
-    #pp es la derivada de P0, y multiplica el lugar que ocupa cada coeficiente del polinomio 
-    #por el valor que tiene
     pp = DERIVAR(p)
-    #Y en pp se "recorren" los lugares, dado que el primer lugar en las listas es 0
-    #Al final, agrega un 0 para mantener el tamaño de la lista inicial
+    #Se agregan a cada lista el resultado de evaluar p1 en los extremos
     valoresizq.append(HORNER(list (reversed(pp)),izq))
     valoresder.append(HORNER(list (reversed(pp)),der))
     #Imprimimos el valor de pp como P1 
     print ("\nP1:\n",pp)
+    #Se inicia el contador de los siguientes Pi polinomios
     i=2
     while True: 
         print ("\nP{:d}:".format(i))
         #En ppp (P2), el último valor de la lista P0, se divide entre el penúltimo de P1 y así sucesivamente
-        #Se hace la división de los elementos de las listas hasta obtener un residuo 0
         ppp = DIVIDIR(p,pp)
         valoresizq.append(HORNER(list (reversed(ppp)),izq))
         valoresder.append(HORNER(list (reversed(ppp)),der))
         print (ppp)
+        #Se incrementa el contador de polinomios en 1, cada vez que se haga una división
         i+=1
+        #Se hace la división de los elementos de las listas hasta obtener una constante como residuo
         if len (ppp)!= 1:
             p = pp 
             pp = ppp
         else:
             break
-    #Se evalúa el extremo más pequeño y el más grande de nuestro intervalo en cada polinomio o lista
+    #Se imprimen las dos listas de los polinomios evaluados en el extremo izquierdo y derecho
     print("\nPolinomios evaluados en {:d}:".format(izq))
     print(valoresizq)
     print("\nPolinomios evaluados en {:d}:".format(der))
     print(valoresder)
-    #Con las sigmas (izq y der) se cuentan los cambios de signo en los valores obtenidos de los polinomios
+    #Se declaran las sigmas (izq y der) se contarán los cambios de signo en los valores obtenidos de los polinomios
     sigmaizq=0
     sigmader=0
     for i in range(len(valoresizq)-1):
@@ -94,9 +92,9 @@ def STURM (p,izq,der):
     for i in range(len(valoresder)-1):
         if valoresder[i]/valoresder[i+1]<0:
             sigmader+=1
-    #La diferencia de los cambios de signo dados por las sigmas, es el número de raíces en el intervalo
+    #El valor absoluto de los cambios de signo dados por las sigmas, es el número de raíces que tiene el polinomio dentro del intervalo
     raices=abs(sigmaizq-sigmader)
     #Imprimimos el número de raíces del polinomio
     print("\nExisten {:d} raíces del polinomio en el intervalo [{:d},{:d}]".format(raices,izq,der))
-
+#Este es nuestro programa principal, donde llamamos a nuestra función principal con los parámetros que se desee
 STURM([-2,1,0,-4,0,0,1],-2,2)
