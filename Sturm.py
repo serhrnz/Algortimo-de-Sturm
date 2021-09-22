17f8a3703ed7395f7cc74dcf9c2b0290e1fd5261
aranzams <89931791+aranzams@users.noreply.github.com>
2021-09-21 22:47:49 -0500

    Update Sturm.py

--- Sturm.py
+++ Sturm.py
@@ -50,18 +50,26 @@ def DIVIDIR (p, pp):
     return p
 
 def STURM (p,izq,der):
+    #Primero se ordenan los coeficientes del polinomio inicial en P0, de menor a mayor grado
     valoresizq=[]
     valoresder=[]
     print ("\nP0:\n",p)
     valoresizq.append(HORNER(list (reversed(p)),izq))
     valoresder.append(HORNER(list (reversed(p)),der))
+    #pp es la derivada de P0, y multiplica el lugar que ocupa cada coeficiente del polinomio 
+    #por el valor que tiene
     pp = DERIVAR(p)
+    #Y en pp se "recorren" los lugares, dado que el primer lugar en las listas es 0
+    #Al final, agrega un 0 para mantener el tamaño de la lista inicial
     valoresizq.append(HORNER(list (reversed(pp)),izq))
     valoresder.append(HORNER(list (reversed(pp)),der))
+    #Imprimimos el valor de pp como P1 
     print ("\nP1:\n",pp)
     i=2
     while True: 
         print ("\nP{:d}:".format(i))
+        #En ppp (P2), el último valor de la lista P0, se divide entre el penúltimo de P1 y así sucesivamente
+        #Se hace la división de los elementos de las listas hasta obtener un residuo 0
         ppp = DIVIDIR(p,pp)
         valoresizq.append(HORNER(list (reversed(ppp)),izq))
         valoresder.append(HORNER(list (reversed(ppp)),der))
@@ -72,11 +80,12 @@ def STURM (p,izq,der):
             pp = ppp
         else:
             break
+    #Se evalúa el extremo más pequeño y el más grande de nuestro intervalo en cada polinomio o lista
     print("\nPolinomios evaluados en {:d}:".format(izq))
     print(valoresizq)
     print("\nPolinomios evaluados en {:d}:".format(der))
     print(valoresder)
-
+    #Con las sigmas (izq y der) se cuentan los cambios de signo en los valores obtenidos de los polinomios
     sigmaizq=0
     sigmader=0
     for i in range(len(valoresizq)-1):
@@ -85,8 +94,9 @@ def STURM (p,izq,der):
     for i in range(len(valoresder)-1):
         if valoresder[i]/valoresder[i+1]<0:
             sigmader+=1
-    
+    #La diferencia de los cambios de signo dados por las sigmas, es el número de raíces en el intervalo
     raices=abs(sigmaizq-sigmader)
+    #Imprimimos el número de raíces del polinomio
     print("\nExisten {:d} raíces del polinomio en el intervalo [{:d},{:d}]".format(raices,izq,der))
 
 STURM([-2,1,0,-4,0,0,1],-2,2)
